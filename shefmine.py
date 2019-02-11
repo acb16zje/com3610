8a2322ecdc1374255d4177440cbe8ca3ebf0b48f
juneezee <engzerjun@gmail.com>
2019-02-11 23:15:52 +0000

    Error handling

--- shefmine.py
+++ shefmine.py
@@ -1,153 +1,14 @@
 import argparse
+import cchardet as chardet
+import difflib
 import git
 import json
+import os
 import re
-import difflib
-import sys
-
-class Vulnerability:
-    vuln_list = []
-
-    def __init__(self, name: str, regex: str) -> None:
-        self.name = name
-        self.regex = re.compile(regex, re.I | re.S)
-        Vulnerability.vuln_list.append(self)
+import vulnerability as vuln
 
 
-# Injection
-injection = Vulnerability(
-    'Injection',
-    '(sql|http header|xxe|nosql|ldap|regex|xpath|xquery|code|queries|xml|html|(shell|os |oper.* sys|command|cmd)|e(-)?mail).*injec|(patch|fix|prevent|found|protect).*(injec| sqli | osci )|(injec|sqli |osci ).*(patch|fix|prevent|found|protect)|(sanitiz).* header(s)?|header(s)?( sanitiz)|quer.* parametriz|(parametriz).* quer'
-)
-
-# Broken Authentication and Session Management
-auth = Vulnerability(
-    'Broken Authentication and Session Management',
-    'brute.*force|sess.*hijack|broken auth|auth.* brok|auth.* bypass|sess.* fixation|(cred|pass|session( )?id|connect).*(plaintext|un(hash|salt|encrypt|safe))|(plaintext|un(hash|salt|encrypt|safe)).*(cred|pass|session( )?id|connect)|(weak|bad|unsafe).* pass.* verif|fix.* (url rewriting|rewriting url)|timeout.*(session|auth.* token)'
-)
-
-# Cross-Site Scripting
-xss = Vulnerability(
-    'Cross-Site Scripting',
-    '(fix|prevent|protect|found|patch).* (xss|cross.*(site|zone) script|script.* attack)|(xss|cross.*(site|zone) script|script.* attack).* (fix|prevent|protect|found|patch)|crlf injec|http resp.* split|(reflect|stored|dom).*xss|xss.*(reflect|stored|dom)|xss (vuln|attack|issue)|(validate|sanitize).* (un(trusted|safe)|malicious)'
-)
-
-# Broken Access Control
-boa = Vulnerability(
-    'Broken Access Control',
-    '(fix|prevent|protect|patch|found).* ((impro.* (auth|access.* control))|url.* access)|((impro.* (auth|access.* control))|url.* access).* (fix|prevent|protect|patch|found)|insec.* direct obj.* ref.*|direct ref.*|auth.* bypass.* control'
-)
-
-# Security Misconfiguration
-smis = Vulnerability(
-    'Security Misconfiguration',
-    '(fix|prevent|protect|patch|found).* ((impro.* (auth|access.* control))|url.* access)|((impro.* (auth|access.* control))|url.* access).* (fix|prevent|protect|patch|found)|insec.* direct obj.* ref.*|direct ref.*|auth.* bypass.* control'
-)
-
-# Sensitive Data Exposure
-sde = Vulnerability(
-    'Sensitive Data Exposure',
-    '(fix|prevent|found|protect|patch).* (man.*in.*midle|mitm|bucket.*brig)|(un|not).*encrypt.* data|(weak|bad|unsafe).*(pass.* hash|key (gener|management))|(important|safe).* header(s)? miss|unsafe.* crypto|(change|update|add).* (https|sec.*cookie.* flag)|rem.* http|(fix|rem).* (secret.*key|hash collision)|(patch|fix|prevent|upgrade|protect).* (sha([- ])?1|md5|md2|md4|(3)?des|collision)'
-)
-
-# Insufficient Attack Protection
-iap = Vulnerability(
-    'Insufficient Attack Protection',
-    '(detect|block|answer|respond|prevent).* (attack|expolit)|(attack|expolit).* (detect|block|answer|respond|prevent)'
-)
-
-# Cross-Site Request Forgery
-csrf = Vulnerability(
-    'Cross-Site Request Forgery',
-    '(fix|prevent|protect|found|patch).*(cross([- ])?site.*(req|ref).*forgery|csrf|sea.*surf|xsrf)|(cross([- ])?site.*(req|ref).*forgery|csrf|sea.*surf|xsrf).*(fix|prevent|protect|found|patch)|(one.*click|autom).*attack|sess.*riding|conf.*deput'
-)
-
-# Using Components with Known Vulnerabilities
-component = Vulnerability(
-    'Using Components with Known Vulnerabilities',
-    '(vuln|(un|not )safe|malicious).* (version|dependenc|component|librar)'
-)
-
-# Underprotected APIs
-upapi = Vulnerability(
-    'Underprotected APIs',
-    '(fix|protect).* api|api.* (fix|protect)|secure.* commun'
-)
-
-# Path / Directory Traversal
-pathtrav = Vulnerability(
-    'Path / Directory Traversal',
-    '((path|dir.*) traver.*|(dot-dot-slash|directory traversal|directory climbing|backtracking).*(attack|vuln))'
-)
-
-# Distributed Denial-of-Service / Denial-of-Service
-dos = Vulnerability(
-    'Distributed Denial-of-Service / Denial-of-Service',
-    '( dos |((distributed)? denial.*of.*service)| ddos |deadlocks)'
-)
-
-# SHA-1 collision
-sha1 = Vulnerability(
-    'SHA-1 Collision',
-    '(sha-1|sha 1|sha1) collision'
-)
-
-# Memory Leaks
-ml = Vulnerability(
-    'Memory Leaks',
-    '(fix|rem|patch|found|prevent) mem.* leak|mem.* leak (fix|rem|patch|found|prevent)'
-)
-
-# Context Leaks
-cl = Vulnerability(
-    'Context Leaks',
-    '(fix|rem|patch|found|prevent).*context leak|context leak.*(fix|rem|patch|found|prevent)'
-)
-
-# Resource Leaks
-rl = Vulnerability(
-    'Resource Leaks',
-    '(fix|rem|patch|found|prevent).* resource.* leaks|resource.* leaks (fix|rem|patch|found|prevent)'
-)
-
-# Overflow
-over = Vulnerability(
-    'Overflow',
-    '(fix|rem|patch|found|prevent).* overflow|overflow.* (fix|rem|patch|found|prevent)'
-)
-
-# Miscellaneous
-misc = Vulnerability(
-    'Miscellaneous',
-    '(fix|found|prevent|protect|patch).*sec.*(bug|vulnerab|problem|defect|warning|issue|weak|attack|flaw|fault|error)|sec.* (bug|vulnerab|problem|defect|warning|issue|weak|attack|flaw|fault|error).*(fix|found|prevent|protect|patch)|vulnerab|attack|cve|nvd|cwe'
-)
-
-# Buffer overflow
-bufover = Vulnerability('Buffer Overflow', 'buff.* overflow')
-
-# Full path disclosure
-fpd = Vulnerability('Full Path Disclosure', '(full)? path discl')
-
-# Null pointers
-nullp = Vulnerability('Null Pointers', 'null pointers')
-
-# Encryption issues
-encrypt = Vulnerability(
-    'Encryption Issues',
-    'encrypt.* (bug|vulnerab|problem|defect|warning|issue|weak|attack|flaw|fault|error)'
-)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('repo', help='Path to the Git repository')
-parser.add_argument('-b', '--branch', help='Select the branch (Default: the active branch of the repository)')
-args = parser.parse_args()
-
-repo = git.Repo(args.repo)
-
-output = {}
-
-
-def process_commit_message(message):
+def process_commit_message(message: str) -> str:
     """
     Pre-process the commit message to remove non-content strings
 
@@ -158,56 +19,134 @@ def process_commit_message(message):
     return re.sub('git-svn-id.*', '', message).strip()
 
 
-# Iterate through all commits in the given branch (default: active branch)
-# for commit in repo.iter_commits(args.branch):
-commit = repo.commit('2bebb2c28cee0708a66b77c2076ef224de4c8bdc')
-for vuln in Vulnerability.vuln_list:
-    commit_message = process_commit_message(commit.message)
-    regex_match = vuln.regex.search(commit_message)
+def search_repository(repo: git.Repo, branch: str) -> dict:
+    """
+    Iterate through all commits of the given repository in the given branch (default: active branch)
 
-    if regex_match is not None:
-        if str(commit) not in output:
-            output[str(commit)] = {}
+    :param repo: The Git repository
+    :param branch: The branch to search
+    """
 
-        # Add vulnerabilities item
-        if 'vulnerabilities' in output[str(commit)]:
-            output[str(commit)]['vulnerabilities'].append(vuln.name)
+    output = {}
+
+    for commit in repo.iter_commits(branch):
+        # return output
+    # commit = repo.commit('2bebb2c28cee0708a66b77c2076ef224de4c8bdc')
+        for vulnerability in vuln.vulnerability_list:
+            commit_message = process_commit_message(commit.message)
+            regex_match = vulnerability.regex.search(commit_message)
+
+            if regex_match is not None:
+                if str(commit) not in output:
+                    output[str(commit)] = {}
+
+                # Add vulnerabilities item
+                if 'vulnerabilities' in output[str(commit)]:
+                    output[str(commit)]['vulnerabilities'].append(vulnerability.name)
+                else:
+                    output[str(commit)]['message'] = commit_message
+                    output[str(commit)]['match'] = regex_match.group()
+                    output[str(commit)]['vulnerabilities'] = [vulnerability.name]
+
+        # Add files changed
+        if str(commit) in output:
+            for diff_item in commit.parents[0].diff(commit):
+                if 'files_changed' not in output[str(commit)]:
+                    output[str(commit)]['files_changed'] = []
+
+                # a (LHS) is None for new file
+                if diff_item.a_blob is None:
+                    a = ''
+                else:
+                    a_stream = diff_item.a_blob.data_stream.read()
+                    a_encoding = chardet.detect(a_stream)
+
+                    if a_encoding['encoding'] is None:
+                        a = a_stream.decode('utf-8', 'replace').splitlines(True)
+                    else:
+                        a = a_stream.decode(a_encoding['encoding'], 'replace').splitlines(True)
+
+                # b (RHS) is None for deleted file
+                if diff_item.b_blob is None:
+                    b = ''
+                else:
+                    b_stream = diff_item.b_blob.data_stream.read()
+                    b_encoding = chardet.detect(b_stream)
+
+                    if b_encoding['encoding'] is None:
+                        b = b_stream.decode('utf-8', 'replace').splitlines(True)
+                    else:
+                        b = b_stream.decode(b_encoding['encoding'], 'replace').splitlines(True)
+
+                diff = difflib.unified_diff(a, b)
+                # print(diff)
+
+                output[str(commit)]['files_changed'].append({
+                    'file': diff_item.a_path,
+                    # 'diff': ''.join(diff)
+                })
+
+    return output
+
+# forward / backward slicing
+# python ghdiff
+# python cdiff
+# python ydiff
+# try sequencematcher in difflib
+# 89fd8d0353f6dc234bf026594c7b4f00caa8dbd8 httpd: this only changes comment
+
+# with open('test.txt', "w") as output:
+#     diff = difflib.unified_diff(a, b)
+#     output.writelines(diff)
+
+
+def output_result(output: dict, path: str):
+    """
+    Output the result into a JSON file
+
+    :param output: The output dictionary
+    :param path: Path (file name) of the output file
+    """
+
+    print(len(output))
+
+    if os.path.isdir(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, 'w') as outfile:
+        json.dump(output, outfile, indent=2)
+
+    print(f'Output result saved to {os.path.realpath(path)}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('repo', help='Path to the Git repository')
+    parser.add_argument('-r', '--revision', help='Select the starting revision '
+                                                 '(Default: the active branch of the repository)')
+    parser.add_argument('-o', '--output', help='Write the result to the specified file name and path '
+                                               '(Default: as output.json in current working directory)')
+    args = parser.parse_args()
+
+    if args.output:
+        output_name, output_extension = os.path.splitext(args.output)
+
+        if output_extension:
+            if output_extension == '.json':
+                output_path = args.output
+            else:
+                print('shefmine.py: Output file extension has been automatically changed to .json')
+                output_path = os.path.realpath(output_name + '.json')
         else:
-            output[str(commit)]['message'] = commit_message
-            output[str(commit)]['match'] = regex_match.group()
-            output[str(commit)]['vulnerabilities'] = [vuln.name]
+            output_path = os.path.realpath(os.path.join(output_name, 'output.json'))
 
-        # for test in commit.diff().iter_change_type('A'):
-        #     print(test)
+    else:
+        output_path = 'output.json'
 
-# Add files changed
-if str(commit) in output:
-    for diff_item in commit.diff(commit.parents[0]):
-        if 'files_changed' not in output[str(commit)]:
-            output[str(commit)]['files_changed'] = []
+    try:
+        output_result(search_repository(git.Repo(args.repo), args.revision), output_path)
+    except git.NoSuchPathError:
+        print(f'shefmine.py: \'{args.repo}\' is not a Git repository')
+    except git.GitCommandError:
+        print(f'shefmine.py: GitCommandError, bad revision \'{args.revision}\'')
 
-        output[str(commit)]['files_changed'].append({
-            'file': diff_item.a_path,
-            'added': 'test'
-        })
-
-
-x = repo.commit('64b98f9945dccbdc1fec085104aa760e37b0e6da').diff('2bebb2c28cee0708a66b77c2076ef224de4c8bdc')[-1]
-a = x.a_blob.data_stream.read().decode('utf-8').splitlines(True)
-b = x.b_blob.data_stream.read().decode('utf-8').splitlines(True)
-# print(a)
-# print()
-# print(b)
-
-
-with open('test.txt', "w") as output:
-    diff = difflib.unified_diff(a, b)
-    output.writelines(diff)
-
-# diff_index = commit.diff(revision+'~1', create_patch=True, ignore_blank_lines=True,
-#                          ignore_space_at_eol=True, diff_filter='cr')
-#
-# print reduce(lambda x, y: str(x)+str(y), diff_index)
-
-# with open('output.json', 'w') as outfile:
-#     json.dump(output, outfile, indent=2)
